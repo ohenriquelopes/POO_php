dc86970cf9dfdbce440f612508fbd3f02198ee18
Henrique l <hlopessantos52@gmail.com>
2022-09-22 10:17:44 -0300

    fix arquivo nao abria navegador

--- www/construtor_destrutor.php
+++ www/construtor_destrutor.php
@@ -1,5 +1,15 @@
 <?php
 
-    
+    class Pessoa {
+
+        public $nome = null;
+
+        function __construct() {
+            echo 'Objeto iniciado';
+        }
+    }
+
+
+    $pessoa = new Pessoa();
 
 ?>
