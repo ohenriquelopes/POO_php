f44c505d965e0533ae6f33e2154a5da36f2dc0f5
Henrique l <hlopessantos52@gmail.com>
2022-09-22 10:47:05 -0300

    finalizado construtor

--- www/construtor_destrutor.php
+++ www/construtor_destrutor.php
@@ -4,12 +4,33 @@
 
         public $nome = null;
 
-        function __construct() {
+        function __construct($nome) {
             echo 'Objeto iniciado';
+            $this->nome = $nome;
+        }
+
+        function __destruct()
+        {
+            echo 'Objeto removido';
+        }
+
+        function __get($atributo) {
+            return $this->$atributo;
+        }
+
+        function correr() {
+            return $this->nome . ' esta correndo'; // ou
+            // return $this->__get('nome') . 'esta correndo';
         }
     }
 
 
-    $pessoa = new Pessoa();
+    $pessoa = new Pessoa('Jorge');
+    echo '<br />Nome: ' . $pessoa->__get('nome');
+    echo '<br />' . $pessoa->correr();
+
+    echo '<br />';
+    // executa o metodo __destruct()
+    unset($pessoa);
 
 ?>
