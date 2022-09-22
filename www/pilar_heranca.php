c8c029faab7730d7c0aa7679be03441df235f088
Henrique l <hlopessantos52@gmail.com>
2022-09-22 11:31:52 -0300

    encerrado heranca

--- www/pilar_heranca.php
+++ www/pilar_heranca.php
@@ -1,12 +1,19 @@
 <?php
 
-    class Carro {
-        public $placa = 'ABC1234';
-        public $cor = 'Branco';
+    //class Carro { //padrao sem heranca
+    class Carro extends Veiculo { // aplicando heranca
+        //public $placa = 'ABC1234';
+        //public $cor = 'Branco';
         public $teto_solar = true;
 
-        function acelerar() {
-            echo 'Acelerar';
+        //function acelerar() {
+        //    echo 'Acelerar';
+        //}
+
+        function __construct($placa, $cor)
+        {
+            $this->placa = $placa;
+            $this->cor = $cor;
         }
 
         function abrirTetoSolar() {
@@ -18,13 +25,19 @@
         }
     }
 
-    class Moto {
-        public $placa = 'DEF1122';
-        public $cor = 'Preta';
+    // class Moto { // sem heranca
+    class Moto extends Veiculo { // aplicando heranca
+        //public $placa = 'DEF1122';
+        //public $cor = 'Preta';
         public $contraPesoGuidao = true;
 
-        function acelerar() {
-            echo 'Acelerar';
+        //function acelerar() {
+        //    echo 'Acelerar';
+        //}
+
+        function __construct($placa, $cor) {
+            $this->placa = $placa;
+            $this->cor = $cor;
         }
 
         function empinar() {
@@ -39,10 +52,33 @@
         function acelerar() {
             echo 'Acelerar';
         }
+
+        function freiar() {
+            echo 'Freiar';
+        }
     }
 
 
-    $carro = new Carro();
-    $moto = new Moto();
+    $carro = new Carro('ABC1234', 'Branco');
+    $moto = new Moto('DEF1234', 'Preta');
+
+    echo '<pre>';
+    print_r($carro);
+    echo '<br /><br />';
+    print_r($moto);
+    echo '</pre>';
+    echo '<hr />';
+
+    $carro->abrirTetoSolar();
+    echo '<br />';
+    $carro->acelerar();
+    $carro->freiar();
+    echo '<br />';
+    echo '<br />';
+
+    $moto->empinar();
+    echo '<br />';
+    $moto->acelerar();
+    $carro->freiar();
 
 ?>
