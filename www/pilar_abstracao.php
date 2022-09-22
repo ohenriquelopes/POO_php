1445f65150ed33df5d4a5eda480f2373ec16e733
Henrique l <hlopessantos52@gmail.com>
2022-09-21 22:29:24 -0300

    alteracoes para getters e setters

--- www/pilar_abstracao.php
+++ www/pilar_abstracao.php
@@ -4,10 +4,26 @@
     class Funcionario {
 
         //atributos
-        public $nome = 'Jose';
-        public $telefone = '11 99999-8888';
-        public $numFilhos = 2;
+        public $nome = null;
+        public $telefone = null;
+        public $numFilhos = null;
 
+        //getters setters
+        function setNome($nome) {
+            $this->nome = $nome;
+        }
+
+        function setNumFilhos($numFilhos) {
+            $this->numFilhos = $numFilhos;
+        }
+
+        function getNome() {
+            return $this->nome;
+        }
+
+        function getNumFilhos() {
+            return $this->numFilhos;
+        }
 
         //metodos
         function resumirCadFunc() {
@@ -26,8 +42,9 @@
     // variavel $y que recebe uma instancia da classe funcionario
     // criando um objeto com base na classe Funcionario() eatribuindo esse OBJ a $y 
     // Isso significa que apartir da $y eu tenho acesso aos atributos e metodos do OBJ
-    $y = new Funcionario();
+    //$y = new Funcionario();
     // Para acessar atributos ou metodos usar -> e na sequencia chamar o atributo ou o metodo
+    /*
     echo $y->resumirCadFunc();
     echo '<br />';
     $y->modificarNumFilhos(3);
@@ -39,4 +56,21 @@
     echo '<br />';
     $x->modificarNumFilhos(1);
     echo $x->resumirCadFunc();
+    */
+
+    $y = new Funcionario();
+    $y->setNome('Jose');
+    $y->setNumFilhos(2);
+    //echo $y->resumirCadFunc();
+    // linha abaixo faz a mesma coisa que a de cima
+    echo $y->getNome() . ' possui ' . $y->getNumFilhos() . ' filho(s)';
+    //echo "$y->getNome() possui $y->getNumFilhos() filhos"; NAO FUNCIONA
+
+    echo '<br />';
+    $x = new Funcionario();
+    $x->setNome('Maria');
+    $x->setNumFilhos(0);
+    echo $x->getNome() . ' possui ' . $x->getNumFilhos() . ' filho(s)';
+
+
 ?>
